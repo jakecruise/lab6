e35c3a37aefac943a2745767664ffa3a99061301
Jake Cruise <jake.cruise@gmail.com>
2021-05-17 14:28:23 -0700

    Added weight converter function

--- lab6.py
+++ lab6.py
@@ -1,9 +1,15 @@
+# Jake Cruise and ...
 
+
+def weight_converter():
+    for weight_kg in range(30,101,10):
+        weight_lb = (weight_kg * 2.2)
+        print(f'Weight in Kilos: {weight_kg:5d} is {weight_lb:6.2f} in Pounds')
 
 
 def main():
-    pass
+    weight_converter()
 
 
-if  __name__ = “__main__”:
+if  __name__ == "__main__":
     main()
